b55ce79ac021a3c7736c59eab83195aadce217df
nate-d3v <nate.d3v@gmail.com>
2022-05-25 17:25:25 -0400

    day 014

---
+++ js/day014.js
@@ -0,0 +1,13 @@
+//You Can't Code Under Pressure #1
+function doubleInteger(i) {
+	return i * 2;
+}
+
+//Jenny's secret message
+function greet(name) {
+	if (name === 'Johnny') {
+		return 'Hello, my love!';
+	} else {
+		return 'Hello, ' + name + '!';
+	}
+}
