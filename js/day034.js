8a7108df22c890f416c65b3a39e057f2be973923
nate-d3v <nate.d3v@gmail.com>
2022-06-14 18:32:31 -0400

    day 034

---
+++ js/day034.js
@@ -0,0 +1,11 @@
+//Volume of a Cuboid
+class Kata {
+	static getVolumeOfCuboid(length, width, height) {
+		return length * width * height;
+	}
+}
+
+//Sum Mixed Array
+function sumMix(x) {
+	return x.reduce((t, n) => t + +n, 0);
+}
