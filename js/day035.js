4285de4ce0ec63054abdb75b39dc907d236ebd4f
nate-d3v <nate.d3v@gmail.com>
2022-06-15 16:22:19 -0400

    day 035

---
+++ js/day035.js
@@ -0,0 +1,9 @@
+//Third Angle of a Triangle
+function otherAngle(a, b) {
+	return 180 - (a + b);
+}
+
+//Remove exclamation marks
+function removeExclamationMarks(s) {
+	return s.split('!').join('');
+}
