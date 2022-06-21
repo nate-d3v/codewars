9efb9df3f6239524c6f3be23be79727ce8452b19
nate-d3v <nate.d3v@gmail.com>
2022-06-21 19:54:49 -0400

    day 041

---
+++ js/day041.js
@@ -0,0 +1,9 @@
+//Twice as old
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+	return Math.abs(dadYearsOld - 2 * sonYearsOld);
+}
+
+//Parse nice int from char problem
+function getAge(inputString) {
+	return Number(inputString.slice(0, 1));
+}
