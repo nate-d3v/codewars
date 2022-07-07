cc296468420b32b1b3831845e7b411aebfb910c9
nate-d3v <nate.d3v@gmail.com>
2022-07-06 20:47:25 -0400

    day 056

---
+++ js/day056.js
@@ -0,0 +1,9 @@
+//All Star Code Challenge #18
+function strCount(str, letter) {
+	return str.split('').filter(x => x === letter).length;
+}
+
+//Grasshopper - Debug sayHello
+function sayHello(name) {
+	return `Hello, ${name}`;
+}
