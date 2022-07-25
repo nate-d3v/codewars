04fae3ef2b216312fcb97cb018b09b981da3aa04
nate-d3v <nate.d3v@gmail.com>
2022-07-24 20:55:48 -0400

    day 074

---
+++ js/day074.js
@@ -0,0 +1,18 @@
+//Find the divisors!
+function divisors(integer) {
+	let arr = [];
+	for (let i = 2; i < integer; i++) {
+		if (integer % i === 0) {
+			arr.push(i);
+		}
+	}
+	return arr.length === 0 ? `${integer} is prime` : arr;
+}
+
+//Reverse words
+function reverseWords(str) {
+	return str
+		.split(' ')
+		.map(x => x.split('').reverse().join(''))
+		.join(' ');
+}
