18954dad555dba1109d82a66375f239a4f9a188d
nate-d3v <nate.d3v@gmail.com>
2022-08-24 18:31:38 -0400

    day 105

---
+++ js/day105.js
@@ -0,0 +1,11 @@
+//Sort arrays - 1
+// input: names - unsorted array
+// output: sorted array
+sortme = function (names) {
+	return names.sort();
+};
+
+//Gauß needs help! (Sums of a lot of numbers).
+function f(n) {
+	return Number.isInteger(n) && n > 1 ? (n * (n - 1)) / 2 + n : false;
+}
