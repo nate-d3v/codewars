f806a653e777160e021de34d2b15a4b2aa67463d
nate-d3v <nate.d3v@gmail.com>
2022-09-20 20:11:12 -0300

    day 132

---
+++ js/day132.js
@@ -0,0 +1,10 @@
+//Multiples of 3 or 5
+function solution(number) {
+	let result = 0;
+	for (let i = 1; i < number; i++) {
+		if (i % 3 === 0 || i % 5 === 0) {
+			result += i;
+		}
+	}
+	return result;
+}
